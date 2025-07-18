62f1d6669311b88a5baafc6ebf8a6aef483d9f27
gokulramesh24 <109064823+gokulramesh24@users.noreply.github.com>
2025-07-18 11:59:00 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,16 @@
+package prac1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+driver.findElement(By.className("btn_action")).click();
+	}
+
+}
